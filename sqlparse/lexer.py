fd070bc489b4b31078fb5e6424979566fa8123d0
Pierre-Samuel LE STANG <ps@lestang.fr>
2015-03-06 15:55:53 +0100

    Support comments starting with '#' character

--- sqlparse/lexer.py
+++ sqlparse/lexer.py
@@ -164,10 +164,10 @@ class Lexer(object):
 
     tokens = {
         'root': [
-            (r'--.*?(\r\n|\r|\n)', tokens.Comment.Single),
+            (r'(--|#).*?(\r\n|\r|\n)', tokens.Comment.Single),
             # $ matches *before* newline, therefore we have two patterns
             # to match Comment.Single
-            (r'--.*?$', tokens.Comment.Single),
+            (r'(--|#).*?$', tokens.Comment.Single),
             (r'(\r\n|\r|\n)', tokens.Newline),
             (r'\s+', tokens.Whitespace),
             (r'/\*', tokens.Comment.Multiline, 'multiline-comments'),
